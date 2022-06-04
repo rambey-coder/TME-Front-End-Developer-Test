67880bf5b44540084feea290648da2f64354142b
Rambey <olayiwolaagboolaakanji@gmail.com>
2022-06-04 12:28:55 +0100

    sidebar done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import './App.css';
+// import HomePage from './HomePage';
 
 function App() {
   return (
     <div className="App">
-      <h1>init</h1>
+      <div className="side-bar">
+        <div className="logo">
+          <h1>Dev<span>Hire</span></h1>
+        </div>
+
+        <div className="navigation">
+          <div className="nav-container active">
+            <span>
+              <i class='bx bx-search'></i>
+              <li>Home</li>
+            </span>
+          </div>
+
+          <div className="nav-container">
+            <span>
+              <i class='bx bx-heart' ></i>
+              <li>Favorites</li>
+            </span>
+          </div>
+        </div>
+      </div>
+      {/* <HomePage /> */}
     </div>
   );
 }
